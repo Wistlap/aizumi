c78125c28129f4003db17a6d7fe6112d251b40e1
miyake13000 <miyake2020@s.okayama-u.ac.jp>
2024-06-25 18:06:43 +0900

    Add template function to respond to Request

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
-use actix_web::{get, post, web, App, HttpServer, Result};
+use actix_web::{post, web, App, HttpServer, Result};
 use serde::{Deserialize, Serialize};
-use serde_json;
+use std::process::exit;
+use std::sync::Mutex;
 
-#[derive(Serialize, Deserialize)]
+type Queue = web::Data<Mutex<Vec<Request>>>;
+
+#[derive(Serialize, Deserialize, Debug)]
 struct Request {
     msg_type: MsgType,
     saddr: i32,
@@ -11,7 +14,7 @@ struct Request {
     payload: String,
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Debug, Default)]
 struct Response {
     code: i32,
     status: String,
@@ -22,8 +25,10 @@ struct Response {
     payload: String,
 }
 
-#[derive(Serialize, Deserialize)]
+#[allow(non_camel_case_types)]
+#[derive(Serialize, Deserialize, Debug, Clone, Copy, Default)]
 enum MsgType {
+    #[default]
     MSG_SEND_REQ = 1,
     MSG_SEND_ACK = 2,
     MSG_RECV_REQ = 3,
@@ -40,24 +45,62 @@ enum MsgType {
     MSG_GBYE_ACK = 14,
 }
 
-#[post("/")]
-async fn submit(info: web::Json<Request>) -> Result<web::Json<Response>> {
-    let res = Response {
-        code: 200,
-        status: String::from("OK"),
-        msg_type: MsgType::MSG_SEND_REQ,
-        saddr: 1,
-        daddr: 100,
-        id: 10000,
-        payload: String::from("hello"),
-    };
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    let queue = web::Data::new(Mutex::new(Vec::<Request>::new()));
+
+    HttpServer::new(move || {
+        App::new()
+            .app_data(queue.clone())
+            .route("/", web::post().to(submit))
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
+}
+
+async fn submit(msg: web::Json<Request>, queue: Queue) -> Result<web::Json<Response>> {
+    let msg = msg.into_inner();
+    let res = treat_msg(msg, queue);
+
     Ok(web::Json(res))
 }
 
-#[actix_web::main]
-async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().service(submit))
-        .bind(("127.0.0.1", 8080))?
-        .run()
-        .await
+fn treat_msg(msg: Request, queue: Queue) -> Response {
+    match msg.msg_type {
+        MsgType::MSG_SEND_REQ => treat_send_req(msg, queue),
+        MsgType::MSG_RECV_REQ => treat_recv_req(msg, queue),
+        MsgType::MSG_FREE_REQ => treat_free_req(msg, queue),
+        MsgType::MSG_PUSH_REQ => treat_push_req(msg, queue),
+        MsgType::MSG_HELO_REQ => treat_helo_req(msg, queue),
+        MsgType::MSG_STAT_REQ => treat_stat_req(msg, queue),
+        MsgType::MSG_GBYE_REQ => treat_gbye_req(msg, queue),
+        _ => {
+            eprintln!("Unexpected MsgType: {:?}", msg.msg_type);
+            exit(1)
+        }
+    }
+}
+
+fn treat_send_req(msg: Request, queue: Queue) -> Response {
+    queue.lock().unwrap().push(msg);
+    Response::default()
+}
+fn treat_recv_req(msg: Request, queue: Queue) -> Response {
+    Response::default()
+}
+fn treat_free_req(msg: Request, queue: Queue) -> Response {
+    Response::default()
+}
+fn treat_push_req(msg: Request, queue: Queue) -> Response {
+    Response::default()
+}
+fn treat_helo_req(msg: Request, queue: Queue) -> Response {
+    Response::default()
+}
+fn treat_stat_req(msg: Request, queue: Queue) -> Response {
+    Response::default()
+}
+fn treat_gbye_req(msg: Request, queue: Queue) -> Response {
+    Response::default()
 }
