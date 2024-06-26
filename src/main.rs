ba8f7a507b8da1f5d31b73872950f67be696c9a7
miyake13000 <miyake2020@s.okayama-u.ac.jp>
2024-06-26 15:02:28 +0900

    Add queue

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
-use actix_web::{post, web, App, HttpServer, Result};
+use actix_web::{web, App, HttpServer, Result};
 use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
 use std::process::exit;
-use std::sync::Mutex;
+use std::sync::{Arc, Mutex, RwLock};
 
-type Queue = web::Data<Mutex<Vec<Request>>>;
+const MY_ID: i32 = 5000;
 
 #[derive(Serialize, Deserialize, Debug)]
 struct Request {
@@ -14,7 +15,7 @@ struct Request {
     payload: String,
 }
 
-#[derive(Serialize, Deserialize, Debug, Default)]
+#[derive(Serialize, Deserialize, Debug)]
 struct Response {
     code: i32,
     status: String,
@@ -23,6 +24,33 @@ struct Response {
     daddr: i32,
     id: i32,
     payload: String,
+}
+
+impl Response {
+    fn new(msg_type: MsgType, saddr: i32, daddr: i32, id: i32, payload: String) -> Self {
+        Self {
+            msg_type,
+            saddr,
+            daddr,
+            id,
+            payload,
+            ..Default::default()
+        }
+    }
+}
+
+impl Default for Response {
+    fn default() -> Self {
+        Self {
+            code: 200,
+            status: String::from("OK"),
+            msg_type: MsgType::default(),
+            saddr: 0,
+            daddr: 0,
+            id: 0,
+            payload: String::default(),
+        }
+    }
 }
 
 #[allow(non_camel_case_types)]
@@ -45,9 +73,49 @@ enum MsgType {
     MSG_GBYE_ACK = 14,
 }
 
+type Queue = web::Data<MsgQueue>;
+
+struct MsgQueue {
+    hash: RwLock<HashMap<i32, Arc<Mutex<Vec<Request>>>>>,
+}
+
+impl MsgQueue {
+    fn new() -> Self {
+        Self {
+            hash: RwLock::new(HashMap::new()),
+        }
+    }
+
+    /// If specified queue_id's queue already exists, enqueue data (Request).
+    /// Else, create queue with specified queue_id, and enqueue data.
+    fn enqueue(&self, data: Request, queue_id: i32) {
+        if let Some(queue) = self.hash.read().unwrap().get(&queue_id) {
+            queue.lock().unwrap().push(data);
+            return;
+        }
+        let queue = Arc::new(Mutex::new(vec![data]));
+        self.hash.write().unwrap().insert(queue_id, queue);
+    }
+
+    /// Dequeue specified id's data (Request) from specified queue_id's queue.
+    /// If that data does not exist, return None.
+    fn dequeue_with_id(&self, queue_id: i32, msg_id: i32) -> Option<Request> {
+        if let Some(queue) = self.hash.read().unwrap().get(&queue_id) {
+            let mut queue = queue.lock().unwrap();
+            if let Some(idx) = queue.iter().position(|msg| msg.id == msg_id) {
+                Some(queue.remove(idx))
+            } else {
+                None
+            }
+        } else {
+            None
+        }
+    }
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    let queue = web::Data::new(Mutex::new(Vec::<Request>::new()));
+    let queue = web::Data::new(MsgQueue::new());
 
     HttpServer::new(move || {
         App::new()
@@ -82,25 +150,89 @@ fn treat_msg(msg: Request, queue: Queue) -> Response {
     }
 }
 
+/// Add msg to queue and return SEND_ACK
 fn treat_send_req(msg: Request, queue: Queue) -> Response {
-    queue.lock().unwrap().push(msg);
-    Response::default()
+    let ack = Response::new(
+        MsgType::MSG_SEND_ACK,
+        MY_ID,
+        msg.saddr,
+        msg.id,
+        String::default(),
+    );
+    let queue_id = msg.daddr;
+    queue.enqueue(msg, queue_id);
+    ack
 }
+
+/// Pop msg from queue and return RECV_ACK
 fn treat_recv_req(msg: Request, queue: Queue) -> Response {
-    Response::default()
+    let msg = queue.dequeue_with_id(msg.saddr, msg.id);
+    if let Some(msg) = msg {
+        Response::new(
+            MsgType::MSG_RECV_ACK,
+            msg.saddr,
+            msg.daddr,
+            msg.id,
+            msg.payload,
+        )
+    } else {
+        Response {
+            code: 404,
+            status: String::from("Not Found"),
+            ..Response::default()
+        }
+    }
 }
-fn treat_free_req(msg: Request, queue: Queue) -> Response {
-    Response::default()
+
+/// Return FREE_ACK
+fn treat_free_req(msg: Request, _queue: Queue) -> Response {
+    Response::new(
+        MsgType::MSG_FREE_ACK,
+        MY_ID,
+        msg.saddr,
+        msg.id,
+        String::default(),
+    )
 }
-fn treat_push_req(msg: Request, queue: Queue) -> Response {
-    Response::default()
+
+/// Not support
+fn treat_push_req(_msg: Request, _queue: Queue) -> Response {
+    Response {
+        code: 400,
+        status: String::from("Bad Request"),
+        ..Response::default()
+    }
 }
-fn treat_helo_req(msg: Request, queue: Queue) -> Response {
-    Response::default()
+
+/// Return HELO_ACK
+fn treat_helo_req(msg: Request, _queue: Queue) -> Response {
+    Response::new(
+        MsgType::MSG_HELO_ACK,
+        MY_ID,
+        msg.saddr,
+        msg.id,
+        String::default(),
+    )
 }
-fn treat_stat_req(msg: Request, queue: Queue) -> Response {
-    Response::default()
+
+/// Return STAT_RES
+fn treat_stat_req(msg: Request, _queue: Queue) -> Response {
+    Response::new(
+        MsgType::MSG_STAT_RES,
+        MY_ID,
+        msg.saddr,
+        msg.id,
+        String::from("stat res"),
+    )
 }
-fn treat_gbye_req(msg: Request, queue: Queue) -> Response {
-    Response::default()
+
+/// Return GBYE_ACK
+fn treat_gbye_req(msg: Request, _queue: Queue) -> Response {
+    Response::new(
+        MsgType::MSG_GBYE_ACK,
+        MY_ID,
+        msg.saddr,
+        msg.id,
+        String::default(),
+    )
 }
