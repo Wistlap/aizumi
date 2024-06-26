1bd25f7f1836d3f8a89695a6904d466b5d99b930
miyake13000 <miyake2020@s.okayama-u.ac.jp>
2024-06-26 16:07:52 +0900

    Add client moc

--- src/bin/client/main.rs
+++ src/bin/client/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello");
+use futures::future::join_all;
+use reqwest::Result;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let works: Vec<_> = (0..1000).map(|i| send_request(i)).collect();
+    let _ret = join_all(works).await;
+    Ok(())
+}
+
+async fn send_request(i: i32) {
+    let json_data =
+        r#"{"msg_type": "MSG_SEND_REQ","saddr": 100,"daddr": 5000,"id": 1,"payload": "hello"}"#;
+    let client = reqwest::Client::new();
+    let _res = client
+        .post("http://localhost:8080/")
+        .header("Content-Type", "application/json")
+        .body(json_data)
+        .send()
+        .await
+        .unwrap();
+
+    println!("{i}");
 }
